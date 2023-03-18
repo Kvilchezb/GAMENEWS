72d1ff7d49caa882646c0164f828cc3e37e603d1
Kyran <Kyran@DESKTOP-MNBLHC3>
2023-03-18 12:48:07 -0600

    Version 1

---
+++ src/main/java/GameNews/GameNews/GameNewsApplication.java
@@ -0,0 +1,13 @@
+package GameNews.GameNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GameNews/GameNews/GameNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package GameNews.GameNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
